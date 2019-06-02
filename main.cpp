06164fde0c3f67982aa949de0550dae6eb73e0a5
Tony <chushuifurong618@126.com>
2019-06-02 21:06:03 +0800

    move window to desktop center

--- main.cpp
+++ main.cpp
@@ -24,5 +24,7 @@ int main(int argc, char *argv[])
     file.close();
     MainWindow w;
     w.show();
+    w.move((QApplication::desktop()->width() - w.width()) / 2,
+         (QApplication::desktop()->height() - w.height()) / 2);
     return a.exec();
 }
